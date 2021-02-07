c96f4105ffc45c83aa4ecdcd187f1bddd21629c8
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-07 22:45:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,12 +13,24 @@ end
 
 
 def list_dwarves(array)
-  array.each_with_index
+  array.each_with_index do 
   
   
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
 end
+
+irb(main):001:0> top10=["ddd","ffgg","dde","edc"]
+=> ["ddd", "ffgg", "dde", "edc"]
+irb(main):002:0> top10.each_with_index do|company,index|
+irb(main):003:1* puts"#{index}.#{company}"
+irb(main):004:1> end
+0.ddd
+1.ffgg
+2.dde
+3.edc
+=> ["ddd", "ffgg", "dde", "edc"]
+irb(main):005:0> 
 
 
