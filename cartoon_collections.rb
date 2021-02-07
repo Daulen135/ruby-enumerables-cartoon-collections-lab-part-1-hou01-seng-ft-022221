de68e34cbf819f236df094affd9a1e447753d88a
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-07 22:49:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,16 +21,5 @@ def list_dwarves(dwarves_array)
   # Print a numbered list of each element
 end
 
-irb(main):001:0> top10=["ddd","ffgg","dde","edc"]
-=> ["ddd", "ffgg", "dde", "edc"]
-irb(main):002:0> top10.each_with_index do|company,index|
-irb(main):003:1* puts"#{index}.#{company}"
-irb(main):004:1> end
-0.ddd
-1.ffgg
-2.dde
-3.edc
-=> ["ddd", "ffgg", "dde", "edc"]
-irb(main):005:0> 
 
 
